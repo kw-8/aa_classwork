69352e758e261fa9f51dac9aa34922f34d78d926
kw-8 <kwu11235813@gmail.com>
2021-09-21 18:04:12 -0700

    more in slideable, knight range, both range

--- week 4/D2/chess/slideable.rb
+++ week 4/D2/chess/slideable.rb
@@ -26,19 +26,32 @@ module Slideable
         self.board[pos].nil? ? range << pos : range = []
       end
     end
+  range
   end
 
   def horizontal_range
     i,j = self.pos
-
+    row = (0..7).map{|idx| [i,idx]}
+    col = (0..7).map{|idx| [idx,j]}
+    endpoints(row) + endpoints(col)
   end
 
   def diagonal_range
+    left  = (0..7).map{|i| [i,i]}
+    right = (0..7).map{|i| [7-i,i]}
+    endpoints(left) + endpoints(right)
   end
 
   def both_range
+    horizontal_range + diagonal_range
   end
 
   def knight_range
+    i,j = self.pos
+    all_spots = [ [i-1, j-2], [i-1, j+2],
+                  [i+1, j-2], [i+1, j+2],
+                  [i-2, j-1], [i-2, j+1],
+                  [i+2, j-1], [i+2, j+1]  ]
+    all_spots.select{|pos| self.board[pos].nil?}
   end
 end
