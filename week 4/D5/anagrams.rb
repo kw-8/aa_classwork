dd78b0bfd9bb081ddddf9b4abe90d380dfc4dadf
jacky <jc6825@stern.nyu.edu>
2021-09-24 19:34:50 -0400

    Complete phase 3

--- week 4/D5/anagrams.rb
+++ week 4/D5/anagrams.rb
@@ -1,7 +1,45 @@
 def first_anagram?(first, second)
-  
+  permutations(first).map {|perm| perm.join("")}.include?(second) 
 end
 
+def permutations(arr)
+  return [arr] if arr.length <= 1
+
+  first = arr.shift 
+  total = []
+  # print arr
+  perms = permutations(arr)
+
+  perms.each do |perm|
+    # print perm
+    (0..perm.length).each do |i|
+      total << perm[0...i] + [first] + perm[i..-1]
+    end 
+  end
+
+  total#.map {|perm| perm.join("")}
+end
+
+p first_anagram?("cat".split(""), "eat")
+p first_anagram?("cat".split(""), "act")
+
+# def permutations(array)
+#   return [array] if array.length <= 1
+
+#   first = array.shift
+
+#   perms = permutations(array)
+#   total = []
+
+#   perms.each do |perm|
+#     (0..perm.length).each do |i|
+#       total << perm[0 ... i] + [first] + perm[i .. -1]
+#     end
+#   end
+#   total
+# end
+
+p permutations("cat".split(""))
 
 
 def second_anagram?(first, second)
@@ -18,3 +56,33 @@ end
 p second_anagram?("tea", "eeat")
 p second_anagram?("tea", "cat")
 p second_anagram?("tea", "eat")
+
+def third_anagram?(string1, string2)
+  quick_sort(string1) == quick_sort(string2)
+end
+
+def quick_sort(arr)
+  return arr if arr.length <= 1 
+  pivot = arr.sample 
+
+  left = []
+  middle = []
+  right = []
+
+  arr.each do |ele|
+    if ele == pivot 
+      middle << ele
+    elsif ele < pivot
+      left << ele
+    else 
+      right << ele
+    end 
+
+  end 
+
+  quick_sort(left) + middle + quick_sort(right)
+end 
+
+p quick_sort([6,5,4,2,2,3,4,5,62, 9])
+puts third_anagram?("apple".split(""), "ppale".split(""))
+
