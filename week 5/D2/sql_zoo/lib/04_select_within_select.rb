274f98e76e6fa485fbaa0f217f37c4ca03fdb515
kw-8 <kwu11235813@gmail.com>
2021-09-28 16:03:51 -0700

    04_select_within_select done

--- week 5/D2/sql_zoo/lib/04_select_within_select.rb
+++ week 5/D2/sql_zoo/lib/04_select_within_select.rb
@@ -35,6 +35,11 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  WHERE population > (SELECT population
+                      FROM countries
+                      WHERE name = 'Russia');
   SQL
 end
 
@@ -42,6 +47,13 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  WHERE gdp/population > (
+    SELECT gdp/population
+    FROM countries
+    WHERE name = 'United Kingdom'
+  );
   SQL
 end
 
@@ -49,6 +61,13 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT name, continent
+  FROM countries
+  WHERE continent IN (
+    SELECT continent
+    FROM countries
+    WHERE name = 'Belize' OR name = 'Belgium'
+  );
   SQL
 end
 
