17d86577298ae6ac0783e544a5099c81de4613b9
kw-8 <kwu11235813@gmail.com>
2021-10-05 16:02:27 -0700

    00, attr accessor done

--- week 6/D2/ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ week 6/D2/ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,11 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each{|name| # :@name < @name is a symbol
+      define_method("#{name}"){ instance_variable_get("@#{name}") }
+      
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end # pass in dummy var
+    }
   end
 end
