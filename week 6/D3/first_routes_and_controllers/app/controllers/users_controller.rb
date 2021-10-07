c8d53effc97223cfeeb9883b8aad34f864e9c913
tharpercp <tharpercp@gmail.com>
2021-10-07 11:55:59 -0700

    finished user_controller methods

--- week 6/D3/first_routes_and_controllers/app/controllers/users_controller.rb
+++ week 6/D3/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,14 +1,40 @@
 class UsersController < ApplicationController
 
     def index
-        render plain: "I'm in the index action!"
+        users = User.all
+        render json: users
     end
 
     def create
-      render json: params
+      user = User.new(user_params) #:user here is the name of the hash. So we need to pass in our values as user[:name]
+      if user.save
+        redirect_to user_url(params[:id])
+      else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+      end
     end
 
     def show
-      render json: params
+      user = User.find(params[:id])
+      render json: user
+    end
+
+    def update
+      user = User.find(params[:id])
+      if user.update(user_params)
+        render json: user
+      else
+        render json: user.errors.full_messages
+      end
+    end
+
+    def destroy
+      user = User.find(params[:id])
+      user.destroy
+      render json: user
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email)
     end
 end
--- week 6/D3/first_routes_and_controllers/app/models/user.rb
+++ week 6/D3/first_routes_and_controllers/app/models/user.rb
@@ -1,10 +1,5 @@
 class User < ApplicationRecord
 
-    def initialize(name, email)
-        @name = name
-        @email = email
-    end
-
     validates :name, :email, presence: true 
 
 end
