7703f2f02773d02ae8d0c31febc2724eecd6eb30
kw-8 <kwu11235813@gmail.com>
2021-11-05 16:57:33 -0700

    render working for tabs (partially)

--- week 10 D5/widgets/widgets.jsx
+++ week 10 D5/widgets/widgets.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './frontend/clock';
-
-let tabsArray = [{title:asdfg, content:fsdaf}]
+import Tabs from './frontend/tabs';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Clock/>, root);
+  ReactDOM.render(<Root/>, root);
 });
+
+const Root = (props) => {
+  return (
+    <div>
+      <Clock/>
+      <Tabs tabsObj={tabsObj}/>
+    </div>
+  )
+  // Tabs props = {tabsObj: [...]}
+}
+
+const tabsObj = [ {title:"1",content:"coconuts"},
+                  {title:"2",content:"macaroons"},
+                  {title:"3",content:"tapioca"}
+                ];
